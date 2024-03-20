045eaaf8651198653eedd21892ccc37f4b4002e6
Alexander Eckert <alexander.eckert@personio.de>
2024-03-20 23:37:35 +0100

    half done with blogposts

---
+++ reading-files/blogposts_test.go
@@ -0,0 +1,24 @@
+package blogposts_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/eckertalex/gobytest/reading-files"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
